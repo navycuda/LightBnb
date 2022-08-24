c0e0fd82871e6859f8246ed9d128b2c4dc24a8e0
NavyCuda <navysupra@gmail.com>
2022-08-24 15:42:38 -0700

    Add properties table

--- migrations/01_schema.sql
+++ migrations/01_schema.sql
@@ -30,11 +30,31 @@ CREATE TABLE
   property_reviews (
     id INTEGER PRIMARY KEY NOT NULL,
     guest_id INTEGER REFERENCES
-      user(id) ON DELETE CASCADE,
+      users(id) ON DELETE CASCADE,
     property_id INTEGER REFERENCES
       properties(id) ON DELETE CASCADE,
     reservation_id INTEGER REFERENCES
       reservations(id) ON DELETE CASCADE,
     rating SMALLINT,
     message TEST
+  );
+CREATE TABLE
+  properties (
+    id INTEGER PRIMARY KEY NOT NULL,
+    owner_id INTEGER REFERENCES
+      users(id) ON DELETE CASCADE,
+    title VARCHAR(255),
+    description TEXT,
+    thumbnail_photo_url VARCHAR(255),
+    cover_photo_url VARCHAR(255),
+    cost_per_night INTEGER,
+    parking_spaces INTEGER,
+    number_of_bathrooms INTEGER,
+    number_of_bedrooms INTEGER,
+    country VARCHAR(255),
+    street VARCHAR(255),
+    city VARCHAR(255),
+    province VARCHAR(255),
+    post_code VARCHAR(255),
+    active BOOLEAN
   );
