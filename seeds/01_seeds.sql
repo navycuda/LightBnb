3317d24096d7e3c37d65d224475f679f03d7b251
NavyCuda <navysupra@gmail.com>
2022-08-24 16:30:12 -0700

    Add user data, incomplete

--- seeds/01_seeds.sql
+++ seeds/01_seeds.sql
@@ -1,3 +1,12 @@
 /*
 \i seeds/01_seeds.sql
 */
+INSERT INTO
+  users (
+    id,
+    name,
+    email,
+    password
+  )
+VALUES
+  (1, 'Eva Stanley', 'sebastianguerra@ymail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.')
