79bb1b53f5b378c540e9bdc25bbe95e8164f8284
NavyCuda <navysupra@gmail.com>
2022-08-24 21:41:44 -0700

    Add query attempt

--- 1_queries/04_most_visited_cities.sql
+++ 1_queries/04_most_visited_cities.sql
@@ -1,3 +1,14 @@
 /*
 \i 1_queries/04_most_visited_cities.sql
 */
+SELECT
+  properties.city,
+  count(reservations)
+FROM
+  properties
+JOIN
+  reservations
+  ON properties.id = reservations.property_id
+ORDER BY
+  properties.city
+;
