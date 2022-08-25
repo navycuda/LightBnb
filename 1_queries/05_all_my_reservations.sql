2f474dabb903e8ccbe28023b668edc79a03bee80
NavyCuda <navysupra@gmail.com>
2022-08-24 22:00:48 -0700

    Add attempt with four tables

--- 1_queries/05_all_my_reservations.sql
+++ 1_queries/05_all_my_reservations.sql
@@ -1,3 +1,25 @@
 /*
 \i 1_queries/05_all_my_reservations.sql
 */
+SELECT
+  reservations.id,
+  properties.title,
+  reservations.start_date,
+  properties.cost_per_night,
+  avg(property_reviews.rating)
+FROM
+  reservations
+JOIN
+  properties 
+  ON reservations.property_id = properties.id
+JOIN
+  property_reviews
+  ON properties.id = property_reviews.property_id
+JOIN
+  users
+  ON reservations.user_id = users.id
+WHERE
+  users.id = 1017016
+GROUP BY
+  properties.title
+;
