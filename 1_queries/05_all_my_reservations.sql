7924cf18d05b0668386d9999722b5ab6a6dfb65f
NavyCuda <navysupra@gmail.com>
2022-08-24 22:11:08 -0700

    Add correct code

--- 1_queries/05_all_my_reservations.sql
+++ 1_queries/05_all_my_reservations.sql
@@ -25,3 +25,28 @@ GROUP BY
   properties.cost_per_night
 ;
 */
+
+SELECT
+  reservations.id,
+  properties.title,
+  properties.cost_per_night,
+  reservations.start_date,
+  avg(rating) AS average_rating
+FROM
+  reservations
+JOIN
+  properties
+  ON reservations.property_id = properties.id
+JOIN
+  property_reviews
+  ON properties.id = property_reviews.property_id
+WHERE
+  reservations.guest_id = 1
+GROUP BY
+  properties.id,
+  reservations.id
+ORDER BY
+  reservations.start_date
+LIMIT
+  10
+;
