619dc6a4b16821c153ab90d8d09e314b72e3a03b
NavyCuda <navysupra@gmail.com>
2022-08-24 21:23:11 -0700

    Added group and order by

--- 1_queries/03_listing_by_city.sql
+++ 1_queries/03_listing_by_city.sql
@@ -5,10 +5,14 @@ SELECT
   properties.id AS id,
   properties.title AS title,
   properties.cost_per_night AS cost_per_night,
-  property_reviews.average_rating AS average_rating
+  avg(property_reviews.average_rating) AS average_rating
 FROM
   properties
 JOIN
   property_reviews
   ON properties.id = property_reviews.property_id
+GROUP BY
+  properties.id
+ORDER BY
+  properties.id
 ;
